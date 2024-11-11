91dee57fd43864a351dbe596080684cdf46dcf57
Adri <abuemor704@g.educaand.es>
2024-11-11 22:18:40 +0100

    README.md

---
+++ suma.py
@@ -0,0 +1,9 @@
+def suma(n1, n2):
+    return n1 + n2
+
+print("Suma: ")
+
+n1 = float(input("Ingrese el primer número: "))
+n2 = float(input("Ingrese el segundo número: "))
+
+print("Resultado: ", suma (n1, n2))
